0999d79b25a729d669d0b91db7a139f04582e54e
Ashim Lugun <146350775+AshimLugun08@users.noreply.github.com>
2025-05-09 15:38:22 +0530

    Update route.ts

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,10 +2,15 @@ import { NextResponse } from 'next/server';
 import { connectDB } from '@/lib/db';
 import User from '@/model/user';
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
   try {
     await connectDB();
-    const user = await User.findById(params.id);
+    const user = await User.findById(id);
     if (!user) {
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
